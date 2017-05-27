0ab31145d4dc9caac514364eb18a9c51546e080e
b606 <besoa606@gmail.com>
2017-05-27 08:43:52 +0200

    Port install dirs to KDE Frameworks 5 TODO : solid/actions untested

--- ClamScan/deinstall.sh
+++ ClamScan/deinstall.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-path="$(kde4-config --path services)"
+path="$(kf5-config --path services)"
 spath="$(echo ${path%:*})"
 rm "$spath"ServiceMenus/ClamScan/ClamScan.desktop
 rm "$spath"ServiceMenus/ClamScan/ClamScan.sh
 rm -r "$spath"ServiceMenus/ClamScan/
-path2="$(kde4-config --localprefix)share/apps/solid/actions"
+path2="$HOME/.local/share/solid/actions"
 rm $path2/ClamScan_action.desktop
+
+# TODO: solid/actions need kbuildsycoca5 after (un)install
+# https://www.kubuntuforums.net/showthread.php?t=68016&p=371184&viewfull=1#post371184
+# Apparently, need to copy to /usr/share/solid/actions/
+# or copy all /usr/share/solid/actions/ to ~/.local/share/solid/actions/
+
+# kbuildsycoca5 --noincremental
--- ClamScan/install.sh
+++ ClamScan/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # PART 1
-# make dir /home/$USER/.kde/share/kde4/services/ServiceMenus/ClamScan
-# and /home/$USER/.kde/share/kde4/services/ServiceMenus/ClamScan/logs/
+# make dir /home/$USER/.local/share/kservices5/ServiceMenus/ClamScan
+# and /home/$USER/.local/share/kservices5/ServiceMenus/ClamScan/logs/
 #
-# copy ClamScan.desktop and ClamScan.sh to /home/$USER/.kde/share/kde4/services/ServiceMenus/ClamScan
-path="$(kde4-config --path services)"
+# copy ClamScan.desktop and ClamScan.sh to /home/$USER/.local/share/kservices5/ServiceMenus/ClamScan
+path="$(kf5-config --path services)"
 spath="$(echo ${path%:*})"
 
 mkdir -p "$spath"ServiceMenus/ClamScan
@@ -13,14 +13,23 @@ mkdir -p "$spath"ServiceMenus/ClamScan/logs
 
 cp ClamScan.desktop "$spath"ServiceMenus/ClamScan/
 cp ClamScan.sh "$spath"ServiceMenus/ClamScan/
-if echo "$spath" | grep '/.kde/share/kde4/services/' > /dev/null; 
+if echo "$spath" | grep '/.local/share/kservices5/' > /dev/null; 
   then :
-  else sed -i -e "s;~/.kde/share/kde4/services/;$spath;g" "$spath"ServiceMenus/ClamScan/ClamScan.desktop; 
+  else sed -i -e "s;~/.local/share/kservices5/;$spath;g" "$spath"ServiceMenus/ClamScan/ClamScan.desktop; 
 fi
 
 # PART 2
-# Copy ClamScan_action.desktop to /home/$USER/.kde/share/apps/solid/actions/
-path2="$(kde4-config --localprefix)share/apps/solid/actions"
+# Copy ClamScan_action.desktop to /home/$USER/.local/share/solid/actions/
+path2="$HOME/.local/share/solid/actions"
+mkdir -p $HOME/.local/share/solid
+mkdir -p $path2
 if [ -d $path2 ]
   then cp ClamScan_action.desktop $path2
 fi
+
+# TODO: solid/actions need kbuildsycoca5 after (un)install
+# https://www.kubuntuforums.net/showthread.php?t=68016&p=371184&viewfull=1#post371184
+# Apparently, need to copy to /usr/share/solid/actions/
+# or copy all /usr/share/solid/actions/ to ~/.local/share/solid/actions/
+
+# kbuildsycoca5 --noincremental
